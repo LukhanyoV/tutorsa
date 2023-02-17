5cc34cfb449f65f256dcbf9ec4ce2904db43d897
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2023-02-17 17:43:00 +0200

    added local database for development

--- db/connect.js
+++ db/connect.js
@@ -2,5 +2,8 @@ const pgp = require("pg-promise")({})
 const config = {
     connectionString: "postgres://twkmabpc:uAn2Gkymi9uqDe3akyfZ7DLP2GINU_py@jelani.db.elephantsql.com/twkmabpc"
 }
+if(process.env.NODE_ENV !== 'production'){
+    config.connectionString = "postgres://tutorsa:s3cur3@localhost:5432/tutorsa"
+}
 const db = pgp(config)
 module.exports = db
