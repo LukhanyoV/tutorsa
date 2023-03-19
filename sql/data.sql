f006bbb557861314833a176228732feda3861b73
Lukhanyo Vakele <lukhanyovakele@gmail.com>
2023-03-19 11:39:20 +0200

    default data for grades and subjects

---
+++ sql/data.sql
@@ -0,0 +1,18 @@
+-- data for grades table
+INSERT INTO grades (grade_name) VALUES ('Grade 8');
+INSERT INTO grades (grade_name) VALUES ('Grade 9');
+INSERT INTO grades (grade_name) VALUES ('Grade 10');
+INSERT INTO grades (grade_name) VALUES ('Grade 11');
+INSERT INTO grades (grade_name) VALUES ('Grade 12');
+
+-- data for subjects
+INSERT INTO subjects (subject_name) VALUES ('Mathematics');
+INSERT INTO subjects (subject_name) VALUES ('Mathematics Literacy');
+INSERT INTO subjects (subject_name) VALUES ('Physical Sciences');
+INSERT INTO subjects (subject_name) VALUES ('Accounting');
+INSERT INTO subjects (subject_name) VALUES ('Life Sciences');
+INSERT INTO subjects (subject_name) VALUES ('Economics');
+INSERT INTO subjects (subject_name) VALUES ('History');
+INSERT INTO subjects (subject_name) VALUES ('Geography');
+INSERT INTO subjects (subject_name) VALUES ('Computer Application Technology');
+INSERT INTO subjects (subject_name) VALUES ('Information Technology');
